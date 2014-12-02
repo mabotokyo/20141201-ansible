1f90889ca5d8d4dc400963f0725bcdf0fc255aa5
Hironobu Saitoh <hironobu-saitoh@gmo.jp>
2014-12-02 09:48:39 +0900

    var_dump()を削除。出力をhtmlspecialchars()を通すようにした

--- lb-setup/index.php
+++ lb-setup/index.php
@@ -1,3 +1,8 @@
+<?php
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+?>
 <!DOCTYPE html>
 <head>
   <meta charset="utf-8">
@@ -8,20 +13,17 @@
   <table>
     <tr>
       <th>接続元アドレス(REMOTE_ADDR)</th>
-      <td><?php echo $_SERVER['REMOTE_ADDR'] ?></td>
+    <td><?php echo h($_SERVER['REMOTE_ADDR']) ?></td>
     </tr>
     <tr>
       <th>接続元のアドレス(X-FORWARDED-FOR)</th>
-      <td><?php echo $_SERVER['HTTP_X_FORWARDED_FOR'] ?></td>
+    <td><?php echo h($_SERVER['HTTP_X_FORWARDED_FOR']) ?></td>
     </tr>
     <tr>
       <th>Webサーバのアドレス(SERVER_ADDR)</th>
-      <td><?php echo $_SERVER['SERVER_ADDR'] ?></td>
+      <td><?php echo h($_SERVER['SERVER_ADDR']) ?></td>
     </tr>
   </table>
-
-  <h3>$_SERVER</h3>
-  <pre><?php var_dump($_SERVER); ?></pre>
 
 </body>
 </html>
